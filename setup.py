806f0910e68b7e6036a565a17b1ba1d3ed6885a2
Alex Lee <alex.j.lee03@gmail.com>
2022-01-05 18:24:45 -0800

    fixed setup

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from setuptools import setup
 setup(
     name=  'bmi203-week1project-AlexLee',
     version= '0.1.1',
-    author= #WHO_ARE_YOU,
-    author_email= #HOW_DO_I_YELL_AT_YOU,
+    author= 'Alex Lee',
+    author_email= 'alex.lee@ucsf.edu',
     packages= [
-		#WHAT_DO_I_CALL_THIS_THING
+		'AlexLee-BMI203-P1'
 		],
-    description= #WHAT_IS_THIS_THING,
+    description='project1',
 	install_requires= [
-		#WHAT_DOES_THIS_NEED_TO_WORK
 		]
 )
